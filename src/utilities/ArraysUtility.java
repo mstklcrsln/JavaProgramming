37d6da9338b3d5edd101bb55daa5237d83a516be
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-08 09:41:54 +0300

    day24 Return Method practice2

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -99,4 +99,51 @@ public class ArraysUtility {
         }
         return result;
     }
+    //	1. create a return method called addInteger that can add an Inteeger  after the  last index of an integer array
+    public static int[] addElement(int[] array, int element) {
+        int[] result = new int[array.length + 1];
+        int i = 0;
+        for (int each : array) {
+            result[i++] = each;
+        }
+        result[i] = element; //element need to be assigned to the last index
+        return result;
+    } // function to add one element to array
+
+    // 2. create a return method called addDouble that can add a double after the last index of a double array
+    public static double[] addElement(double[] array, double element) {
+
+        double[] result = new double[array.length + 1];
+
+        int i = 0;
+        for (double each : array) {
+            result[i++] = each;
+
+        } result[i] = element;//element need to be assigned to the last index
+        return result;
+    }
+
+    // 3. create a return method called addString that can add a String after the last index of a String array
+    public static String [] addElement (String [] array,String element){
+        String [] result= new String[array.length+1];
+        int i =0;
+        for (String each : array) {
+            result[i++]=each;
+        }
+        result[i]=element;//element need to be assigned to the last index
+        return result;
+
+    }
+
+    // 4. create a return method called addChar that can add a char after last index of a char array
+    public static char[] addElement (char[] array, char element){
+        char [] result = new char[array.length+1];
+        int i=0;
+        for (char each : array) {
+            result[i++]= each;
+        }
+        // result[result.length-1]= element; we can write like this
+        result[i]= element; //i is the last index of array
+        return result;
+    }
 }
