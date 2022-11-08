38de9caa37f1e2fcdd203f432400161b181b9931
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-08 10:42:12 +0300

    day24 Return Method practice2

--- src/day26_CustomMethodPractice/UniqueElements.java
+++ src/day26_CustomMethodPractice/UniqueElements.java
@@ -12,18 +12,42 @@ public class UniqueElements {
        int [] unique= uniqueElements(array);
         System.out.println(Arrays.toString(unique)); //[5, 8] are unique elements
 
+        System.out.println("-----------------");
+
+        double [] doubleArray = {11.5,11.5,11.5,10.5,5.5,5.5,5.5,5.5,4.5};
+
+        double [] uniq= uniqueElements(doubleArray);
+
+        System.out.println(Arrays.toString(uniq)); //[10.5, 4.5]
+
+        System.out.println("-----------------");
+
 
     }
-
+        // Returns the unique elements of the array as a new array
     public static int [] uniqueElements (int [] array){
         int [] result = {};  // new int [0]
         for (int each : array) {
-         if(ArraysUtility.frequencyOfElement(array,each)==1) {
+         if(ArraysUtility.frequencyOfElement(array,each)==1) { // if the frequence is 1 that means the element is uniq.
            result= ArraysUtility.addElement(result,each);
          }
         }
         return result;
         }
+
+    // Returns the unique doubles of the array as a new array
+    public static double [] uniqueElements (double [] array){
+        double [] result = {};  // new int [0]
+        for (double each : array) {
+            if(ArraysUtility.frequencyOfElement(array,each)==1) { // if the frequence is 1 that means the element is uniq.
+                result= ArraysUtility.addElement(result,each);
+            }
+        }
+        return result;
+    }
+
+
+
     }
 
 
