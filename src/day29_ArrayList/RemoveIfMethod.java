9a96f0b2aa7af0b63442f20fa9c44cbb846219a3
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-14 17:29:30 +0300

    Day29 ArrayList

--- src/day29_ArrayList/RemoveIfMethod.java
+++ src/day29_ArrayList/RemoveIfMethod.java
@@ -14,9 +14,9 @@ public class RemoveIfMethod {
 
         // System.out.println(list); [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        // to remove the odd numbers,
+       /* to remove the odd numbers,
 
-       /* for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             if (list.get(i)%2!=0){
               //  list.remove(i); // removes by index. But if we use remove method in the loop we can not get the right result, because index numbers are changing in every loop
                 //   System.out.println(list); [2, 4, 6, 8, 1, 2, 4, 6, 8, 1, 2, 4, 6, 8]*/
@@ -24,9 +24,7 @@ public class RemoveIfMethod {
       /* lambda expression: we will use lambda expression to remove the objects they are matching with the specific condition
         p is the syntax of lambda expression p represents every single element of the list
         next we give lambda expression (  -> ) after that we give a boolean
-
        */
-
        list.removeIf(p -> p<5); //the elements smaller than 5
 
         System.out.println(list); //[5, 6, 7, 8, 9, 5, 6, 7, 8, 9, 5, 6, 7, 8, 9]       all the elements less than 5
@@ -38,11 +36,5 @@ public class RemoveIfMethod {
         // i want to remove all the even numbers from the ArrayList
         list2.removeIf(each-> each%2==0);
         System.out.println(list2); //[1, 3, 5, 11, 33, 55, 77, 9, 111]
-
-
-
-
-
-
     }
 }
