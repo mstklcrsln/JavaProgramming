17c761e4750bd525c3fea290cf549ee5d75d7bba
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-14 12:55:56 +0300

    Day28 ArrayListStudies

---
+++ src/day28_ArrayList/UniqueElements.java
@@ -0,0 +1,40 @@
+package day28_ArrayList;
+
+import java.util.ArrayList;
+
+public class UniqueElements {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        // to find uniq elements and add them to a new ArrayList
+        list.add("Java");
+        list.add("Java");
+        list.add("Java");
+        list.add("Python");
+        list.add("C#");
+        list.add("C#");
+        list.add("Ruby");
+        list.add("C#");
+        list.add("C++");
+        list.add("C++");
+
+        System.out.println(list);
+
+        ArrayList<String > unique= new ArrayList<>();
+        // to find the uniq element we need to iterate the list from first to the last element
+        //we can use for each loop
+
+        for (String each:list){
+            if (list.indexOf(each) == list.lastIndexOf(each)){
+                unique.add(each);
+            }
+        }
+        System.out.println(unique); //[Python, Ruby] the unique elements of array
+
+
+
+
+
+
+    }
+}
