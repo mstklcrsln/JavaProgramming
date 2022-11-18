5091abd193e0760e72ad7d071691e0ffe8227d04
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-18 20:41:15 +0300

    Day30 CustomClass

--- src/day30_CustomClass/StudentObjects.java
+++ src/day30_CustomClass/StudentObjects.java
@@ -1,5 +1,8 @@
 package day30_CustomClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentObjects {
     public static void main(String[] args) {
 
@@ -23,5 +26,34 @@ public class StudentObjects {
 
         Student [] students = {student1,student5,student2,student3,student4};
 
+       /* System.out.println(Arrays.toString(students));
+       [Student{name='Mesut', gender=M, age=41, ID=103, grade=A}, Student{name='Zülal', gender=F, age=29, ID=125, grade=B}, Student{name='Serkan', gender=M, age=39, ID=105, grade=B}, Student{name='Veli', gender=M, age=42, ID=109, grade=C}, Student{name='Fatma', gender=F, age=32, ID=117, grade=D}]
+        */
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+            /*
+            Student{name='Mesut', gender=M, age=41, ID=103, grade=A}
+            Student{name='Zülal', gender=F, age=29, ID=125, grade=B}
+            Student{name='Serkan', gender=M, age=39, ID=105, grade=B}
+            Student{name='Veli', gender=M, age=42, ID=109, grade=C}
+            Student{name='Fatma', gender=F, age=32, ID=117, grade=D}
+             */
+
+            System.out.println("-----------");
+            ArrayList<Student> earlyBirds= new ArrayList<>(); // grade A
+            ArrayList<Student> angryBirds= new ArrayList<>();
+
+        // to find early and angry birds
+        for (Student student : students) {
+            if (student.grade=='A'){ // if grade is a add to the early birds
+                earlyBirds.add(student);
+                } else { angryBirds.add(student);
+                }
+            }
+        System.out.println(earlyBirds); //[Student{name='Mesut', gender=M, age=41, ID=103, grade=A}]
+        System.out.println(angryBirds); //[Student{name='Zülal', gender=F, age=29, ID=125, grade=B}, Student{name='Serkan', gender=M, age=39, ID=105, grade=B}, Student{name='Veli', gender=M, age=42, ID=109, grade=C}, Student{name='Fatma', gender=F, age=32, ID=117, grade=D}]
+        }
     }
-}
+
