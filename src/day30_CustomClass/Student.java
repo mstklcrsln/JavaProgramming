a9b22f7ecfe708947d5ab2cbd81629875f759035
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-18 18:41:32 +0300

    Day30 CustomClass

--- src/day30_CustomClass/Student.java
+++ src/day30_CustomClass/Student.java
@@ -8,7 +8,7 @@ public class Student {
     public int ID;
     public char grade;
 
-    public void setInfo (String name, char gender, int age, int ID, char grade){
+   /* public void setInfo (String name, char gender, int age, int ID, char grade){
       // this keyword is sing to call the instance variable
         this.name= name;
         this.gender= gender;
@@ -16,8 +16,17 @@ public class Student {
         this.ID= ID;
         this.grade= grade;
 
-    }
+     automatic way of using setIfo method
+            right click+ generate+ constructor+ choose all+ Ok
+    */
 
+    public void setInfo (String name, char gender, int age, int ID, char grade) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.ID = ID;
+        this.grade = grade;
+    }
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
