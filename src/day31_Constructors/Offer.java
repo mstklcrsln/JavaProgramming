12b49afd76a67c5899e0db0a2c1a30aadb2e1180
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-21 20:40:28 +0300

    Day31 CustomClass_Constructors

---
+++ src/day31_Constructors/Offer.java
@@ -0,0 +1,42 @@
+package day31_Constructors;
+
+public class Offer {
+
+    public String location, companyName, jobTitle;
+    public double salary;
+    public boolean hasBenefit, hasPTO, isWHF,isFullTime;
+
+
+    public Offer(String location, String companyName, String jobTitle, double salary, boolean hasBenefit, boolean hasPTO, boolean isWHF, boolean isFullTime) {
+        this.location = location;
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.hasBenefit = hasBenefit;
+        this.hasPTO = hasPTO;
+        this.isWHF = isWHF;
+        this.isFullTime = isFullTime;
+    }
+
+    public String toString() {
+        return "Offer{" +
+                "location='" + location + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                ", hasBenefit=" + hasBenefit +
+                ", hasPTO=" + hasPTO +
+                ", isWHF=" + isWHF +
+                ", isFullTime=" + isFullTime +
+                '}';
+    }
+}
+/*
+1. Create a custom class named Offer
+				Attributes:
+						location, companyName, jobTitle, salary, hasBenefit, hasPTO, isWFH, isFullTime
+
+				Actions:
+					setInfo(): sets all the instance variables
+					toString(): returns the full info of the Offer Object
+ */
