2aadd02ccdb10f275f002ae561d4b4fee36336c7
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-23 14:15:20 +0300

    Day33 CustomClass_Statics

---
+++ src/day33_CustomClass_Statics/IPhone.java
@@ -0,0 +1,18 @@
+package day33_CustomClass_Statics;
+
+public class IPhone {
+
+    public static String brand= "Apple";            // static, same for all objects
+    public String model;                            // Instance, changable for all objects
+    public double price;                            // Instance, changable for all objects
+    public static String operatingSystem = "IOS";   // static, same for all objects
+    public String color;                            // Instance, changable for all objects
+    public String size;                             // Instance, changable for all objects
+    public static String madeIn = "China";          // static, same for all objects
+    public static boolean hasBattery = true;        // static, same for all objects
+    public static boolean isTouchScreen = true;     // static, same for all objects
+    public static boolean hasFaceTime = true;       // static, same for all objects
+
+
+
+}
