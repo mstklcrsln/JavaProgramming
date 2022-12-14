95732fae3a9c401bed952eb33905f7bdfb8c5e7b
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-12-14 13:15:49 +0300

    Last updates

--- src/day38_MethodOverriding/carTask/Car.java
+++ src/day38_MethodOverriding/carTask/Car.java
@@ -2,4 +2,37 @@ package day38_MethodOverriding.carTask;
 
 public class Car {
 
+    public String brand, model, color;
+    public int year, miles;
+    public double price;
+
+    public Car(String brand, String model, String color, int year, int miles, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.miles = miles;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", miles=" + miles +
+                ", price=" + price +
+                '}';
+    }
+
+    public void start (){
+        System.out.println(brand+  " "+ model + " is starting");
+    }
+
+    public void drive (){
+        System.out.println(brand+  " "+ model + " is driving");
+    }
+
+
 }
