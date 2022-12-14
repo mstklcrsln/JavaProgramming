6fd418937422d732be1d3730a63db3a3f5ecd8ca
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-12-14 14:22:09 +0300

    Last updates

---
+++ src/day38_MethodOverriding/carTask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_MethodOverriding.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", "White", 2022,65000,22000);
+
+        Tesla tesla = new Tesla("Super", "Red", 2019,20000,52000);
+        BMW bmw =new BMW("3.20", "Yellow", 2015,25000,35000);
+
+    }
+}
---
+++ src/day38_MethodOverriding/carTask/ToyotaObjects.java
@@ -0,0 +1,12 @@
+package day38_MethodOverriding.carTask;
+
+public class ToyotaObjects {
+    public static void main(String[] args) {
+
+
+        Toyota toyota1 = new Toyota("Corolla", "Grey",2017,85000,25000);
+
+        toyota1.reliable(); //Toyota Corolla is reliable
+
+    }
+}
