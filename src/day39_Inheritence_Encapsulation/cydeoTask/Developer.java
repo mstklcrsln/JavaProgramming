3cd72d4e316264b213a4f95094d635943b738a4c
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-12-15 17:42:30 +0300

    Day 39 Cydeo Task

--- src/day39_Inheritence_Encapsulation/cydeoTask/Developer.java
+++ src/day39_Inheritence_Encapsulation/cydeoTask/Developer.java
@@ -1,4 +1,23 @@
 package day39_Inheritence_Encapsulation.cydeoTask;
 
-public class Developer {
+public class Developer extends Employee {
+
+    public Developer(String name, int age, char gender, String jobTitle, int employeeID, double salary) {
+        super(name, age, gender, jobTitle, employeeID, salary);
+    }
+
+    public void fixingBugs (){
+        System.out.println(getName()+ " is fixing the bugs");
+        }
+
+    @Override
+    public void work() {
+        System.out.println(getName() + " is working for developing app");
+    }
 }
+/*
+ 4. Create a sub class of Employee named Developer
+            Override the work method
+            Extra methods:
+                fixingBugs()
+ */
