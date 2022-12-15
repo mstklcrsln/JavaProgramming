2cdbc25c69b0afdc7b780e23072c5dcf57903749
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-12-15 17:38:34 +0300

    Day 39 Cydeo Task

--- src/day39_Inheritence_Encapsulation/cydeoTask/Tester.java
+++ src/day39_Inheritence_Encapsulation/cydeoTask/Tester.java
@@ -1,4 +1,26 @@
 package day39_Inheritence_Encapsulation.cydeoTask;
 
-public class Tester {
+public class Tester extends Employee {
+
+    public Tester(String name, int age, char gender, String jobTitle, int employeeID, double salary) {
+        super(name, age, gender, jobTitle, employeeID, salary);
+    }
+
+    public void  createTicket(){
+        System.out.println(getName()+ " " + getJobTitle()+ " is creating ticket");
+    }
+
+    @Override
+    public void work() {
+        //super.work(); we can change these point. we can give different implementation here
+        System.out.println(getName()+ " is testing the application");
+    }
 }
+/*
+ 3. Create a subclass of Employee named Tester
+
+            Override the work method
+
+            Extra methods:
+                createTicket()
+ */
