49e449d2f1a5a8ce3acbf025eb1ef3049438cf1b
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-12-22 15:00:16 +0300

    Day 41 Exceptions try&catch blocks

--- src/day41_ExceptionsIntro/TryCatchBlocks.java
+++ src/day41_ExceptionsIntro/TryCatchBlocks.java
@@ -20,10 +20,11 @@ public class TryCatchBlocks {
 
         System.out.println("Test started");
         try { // in the try block we will give our exception code
-            System.out.println(9/0);
+            System.out.println(9 / 0);
 
             System.out.println("Try block");
-        } catch ( ArithmeticException e){ // we put the ArithmeticException and add e, we need to specify that which class can be used for handling that exception.
+        } catch (
+                ArithmeticException e) { // we put the ArithmeticException and add e, we need to specify that which class can be used for handling that exception.
             // e is a variable. variable e will contain the object of the exception that is occured.
             // every time exception is occured, exception object will be created
             // and than that object will be assigned the variable type that we give here
@@ -63,10 +64,10 @@ public class TryCatchBlocks {
          */
 
         System.out.println("Test 2 started");
-        int [] numbers = {1,2,3,4,5};
+        int[] numbers = {1, 2, 3, 4, 5};
         try {
             System.out.println(numbers[200]);
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             //if we write catch (NullPointerException e){ we can not handle the exception so we need to use right exceptionclass
             System.out.println("catch block");
         }
@@ -79,6 +80,79 @@ public class TryCatchBlocks {
         catch block
         Test2 completed
          */
+        System.out.println("----------------------------");
 
+        /*
+        if we don't know the class we use parent class: RunTimeException Class
+         */
+        System.out.println("Test 3 started");
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        try {
+            System.out.println(numbers1[200]);
+        } catch (RuntimeException e) { // we used parent class RuntimeException
+            System.out.println("catch block");
+        }
+
+        System.out.println("Test3 completed");
+        /*
+        writes in the console:
+
+        Test 3 started
+        catch block
+        Test3 completed
+         */
+
+        System.out.println("---------------------");
+
+        /*
+        in the catch block there are some specific methods that we can call from the exception object.
+
+        When runtime exception happens, java will catch it and assigns to a variable in catch block
+        • After it is successfully caught, we can use the variable and call some methods on the
+        exception object
+                • Popular methods of exception objects are:
+                printStackTrace(): prints a stack trace (full details) of the exception
+                • getMessage(): returns only brief description of the exception
+         */
+        System.out.println("Test 4 started");
+        int[] numbers2 = {1, 2, 3, 4, 5};
+        try {
+            System.out.println(numbers2[200]);
+        } catch (RuntimeException e) { // we used parent class RuntimeException
+            e.printStackTrace(); // this method is very efficient for reports
+        }
+        System.out.println("Test 4 completed");
+        /*
+         writes in the console: after write everything and after that explains the exception. Not terminates the programme
+
+        Test 4 started
+        Test 4 completed
+        java.lang.ArrayIndexOutOfBoundsException: Index 200 out of bounds for length 5
+            at day41_ExceptionsIntro.TryCatchBlocks.main(TryCatchBlocks.java:120)
+
+        Process finished with exit code 0
+         */
+
+        System.out.println("---------------------------------");
+
+        System.out.println("Test 5 started");
+        int[] numbers3 = {1, 2, 3, 4, 5};
+        try {
+            System.out.println(numbers3[200]);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage()); // it gives us Index 200 out of bounds for length 5
+        }
+        System.out.println("Test 5 completed");
     }
+    /*
+     writes in the console:
+
+        Test 5 started
+        Index 200 out of bounds for length 5
+        Test 5 completed
+        java.lang.ArrayIndexOutOfBoundsException: Index 200 out of bounds for length 5
+            at day41_ExceptionsIntro.TryCatchBlocks.main(TryCatchBlocks.java:120)
+
+        Process finished with exit code 0
+     */
 }
