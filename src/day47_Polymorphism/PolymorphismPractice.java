f07547556d31d21137fe9d8745b55efbd1e5f696
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2023-01-17 10:42:56 +0300

    Day 49 Collections and last updates

--- src/day47_Polymorphism/PolymorphismPractice.java
+++ src/day47_Polymorphism/PolymorphismPractice.java
@@ -1,51 +1,53 @@
-/*package day47_Polymorphism;
+package day47_Polymorphism;
 
 import day38_MethodOverriding.carTask.BMW;
+import day38_MethodOverriding.carTask.Car;
+import day38_MethodOverriding.carTask.Tesla;
 import day38_MethodOverriding.carTask.Toyota;
 
 public class PolymorphismPractice {
-    import day38_Inheritance.carTask.BMW;
-import day38_Inheritance.carTask.Car;
-import day38_Inheritance.carTask.Tesla;
-import day38_Inheritance.carTask.Toyota;
-
-    public class PolymorphismPractice {
-
         public static void main(String[] args) {
 
             Car[] cars = {
-                    new Toyota("Highlander", 2010, 25000, "White", 255000),
-                    new BMW("X5", 2014, 32000, "Red", 12000),
-                    new Toyota("Corolla", 2011, 20000, "White", 310000),
-                    new BMW("M3", 2015, 33000, "Blue", 14030),
-                    new BMW("M5", 2017, 35000, "Black", 15000),
-                    new BMW("M4", 2018, 40000, "White", 19000),
-                    new BMW("7 Series", 2009, 30000, "Purple", 21000),
-                    new BMW("i3", 2011, 30000, "Black", 9000),
-                    new Toyota("Camry", 2018, 30000, "Red", 185000),
-                    new Toyota("Rav4", 2012, 23000, "Red", 285000),
-                    new Toyota("Avalon", 2019, 35000, "Blue", 175230),
-                    new Tesla("Model 3", 2015, 45000, "White", 235000),
-                    new Tesla("Model Y", 2017, 65000, "Black", 135000),
-                    new Tesla("Model X", 2016, 48000, "White", 235000),
-                    new Tesla("Model X", 2014, 48000, "White", 236000),
+                  /*  new Toyota("Highlander", "2010", 25000, "5", 255000),
+                    new BMW("X5", "2014", 32000, 6, 12000),
+                    new Toyota("Corolla", "2011", 20000, 1, 310000),
+                    new BMW("M3", "2015", 33000, "Blue", 14030),
+                    new BMW("M5", "2017", 35000, "Black", 15000),
+                    new BMW("M4", "2018", 40000, "White", 19000),
+                    new BMW("7 Series", "2009", 30000, "Purple", 21000),
+                    new BMW("i3", "2011", 30000, "Black", 9000),
+                    new Toyota("Camry", "2018", 30000, "Red", 185000),
+                    new Toyota("Rav4", "2012", 23000, "Red", 285000),
+                    new Toyota("Avalon", "2019", 35000, "Blue", 175230),
+                    new Tesla("Model 3", "2015", 45000, "White", 235000),
+                    new Tesla("Model Y", "2017", 65000, "Black", 135000),
+                    new Tesla("Model X", "2016", 48000, "White", 235000),
+                    new Tesla("Model X", "2014", 48000, "White", 236000),*/
+
             };
 
+            /*
+              1.1 Write a program that can display all the cars that are eligible for recall
+              Cars that are eligible for recall:
+	        							Toyota: from year 2010 to 2011
+	        							BMW: from year 1929 to 2022
+	        							Tesla: from year 2015-2016
+             */
+                for (Car eachCar : cars) {
 
-            for(Car eachCar  : cars ){
-
-                if(eachCar instanceof Toyota){
-                    if(eachCar.year >= 2010 && eachCar.year <= 2011){
+                if (eachCar instanceof Toyota) {
+                    if (eachCar.year >= 2010 && eachCar.year <= 2011) {
                         System.out.println(eachCar);
                     }
                 }
 
-                if(eachCar instanceof BMW){
+                if (eachCar instanceof BMW) {
                     System.out.println(eachCar);
                 }
 
-                if(eachCar instanceof Tesla){
-                    if(eachCar.year >= 2015 && eachCar.year <= 2016){
+                if (eachCar instanceof Tesla) {
+                    if (eachCar.year >= 2015 && eachCar.year <= 2016) {
                         System.out.println(eachCar);
                     }
                 }
@@ -60,15 +62,15 @@ import day38_Inheritance.carTask.Toyota;
 
 
             Car carWithHighestMileage = cars[0],
-                    carWithLowestMileage =  cars[0];
+                    carWithLowestMileage = cars[0];
 
 
             for (Car eachCar : cars) {
-                if(eachCar.miles > carWithHighestMileage.miles){
+                if (eachCar.miles > carWithHighestMileage.miles) {
                     carWithHighestMileage = eachCar;
                 }
 
-                if(eachCar.miles < carWithLowestMileage.miles){
+                if (eachCar.miles < carWithLowestMileage.miles) {
                     carWithLowestMileage = eachCar;
                 }
 
@@ -77,10 +79,7 @@ import day38_Inheritance.carTask.Toyota;
             System.out.println(carWithHighestMileage);
             System.out.println(carWithLowestMileage);
 
-
-
-
-        }*/
+        }}
 
 
 
---
+++ src/day48_Collections/ListIntro.java
@@ -0,0 +1,12 @@
+package day48_Collections;
+import java.util.ArrayList;
+import java.util.List;
+public class ListIntro {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+
+    }
+
+}
